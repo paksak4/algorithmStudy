547431d1d194f5d55f7f46d8c1ae082ac65e51c3
paksak4 <paksak4@naver.com>
2025-06-07 21:37:17 +0900

    bfs로 해결, 뒤에서부터 접근하면 그리디로도 해결 가능

---
+++ 16953.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(void)
+{
+    long long a, b;
+    cin>>a>>b;
+    queue<pair<long, long>> que;
+    que.push({a, 1});
+
+    while(!que.empty())
+    {
+        pair<int, int> p = que.front();
+        que.pop();
+
+        if(p.first*2==b || p.first*10+1==b)
+        {
+            cout<<p.second+1;
+            return 0;
+        }
+
+        if(p.first*2<b)
+            que.push({p.first*2, p.second+1});
+        if(p.first*10+1<b)
+            que.push({p.first*10+1, p.second+1});
+    
+    }
+    cout<<"-1";
+    return 0;
+}
